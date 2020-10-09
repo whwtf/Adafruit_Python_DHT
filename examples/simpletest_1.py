ef96d34a6790058355b8f54f22125998100940c9
whwtf <notfun@139.com>
2020-10-09 21:26:03 +0800

    new commit

--- examples/simpletest_1.py
+++ examples/simpletest_1.py
@@ -25,6 +25,7 @@ import Adafruit_DHT
 # Sensor should be set to Adafruit_DHT.DHT11,
 # Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
 sensor = Adafruit_DHT.DHT22
+sensor2 = Adafruit_DHT.DHT11
 
 # Example using a Beaglebone Black with DHT sensor
 # connected to pin P8_11.
@@ -33,16 +34,20 @@ sensor = Adafruit_DHT.DHT22
 # Example using a Raspberry Pi with DHT sensor
 # connected to GPIO23.
 pin = 18
+pin2 = 25
 
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+humidity2, temperature2 = Adafruit_DHT.read_retry(sensor2, pin2)
 
 # Note that sometimes you won't get a reading and
 # the results will be null (because Linux can't
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
-if humidity is not None and temperature is not None:
+if humidity is not None and temperature is not None and humidity2 is not None and temperature2 is not None:
     print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+    print('Temp2={0:0.1f}*C  Humidity2={1:0.1f}%'.format(temperature2, humidity2))
 else:
     print('Failed to get reading. Try again!')
+
